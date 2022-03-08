fcda86e86bb056bbe2b953d804e658edff82ea0b
Mohamed-Gamal-Mubarek <mohamedgamalmubarek@gmail.com>
2022-03-08 16:49:39 +0200

    make migration and migrate and create model job and makemigration and migaret

--- job-board/job/admin.py
+++ job-board/job/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+# IMPORT MODEL TO REGISTER IN ADMIN
+from .models import Job
 # Register your models here.
+admin.site.register(Job)
+
--- job-board/job/models.py
+++ job-board/job/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# CREATE TABLE 
+class Job(models.Model):
+    #COLUMN TITLE
+    title = models.CharField(max_length=100) 
