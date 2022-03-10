2b17012def483d8acc508db824d742612ae22dfc
Mohamed-Gamal-Mubarek <mohamedgamalmubarek@gmail.com>
2022-03-10 12:38:08 +0200

    write some comments in job view

--- job-board/job/views.py
+++ job-board/job/views.py
@@ -7,8 +7,16 @@ from .models import Job
 
 
 def jop_list(request):
+    # GET ALL JOB BY THIS
     jobList = Job.objects.all()
+    # GET ALL JOB 
+
+    # STORE ALL JOB IN CONTEXT 
     context = {'jobs': jobList}
+    # STORE ALL JOB IN CONTEXT
+
+    # RETURN  ALL JOB TO RENDER DATA => RENDER DATA * , TEMPLATE YOU WANT TO VIEW , CONTEXT => SOTORE DATA
+    # DATA IS ALL JOBS HERE
     return (render(request, 'job/jobs.html', context))
 
 def jop_details(request, id):
