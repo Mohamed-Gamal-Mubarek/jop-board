a8da888af2412038a6971bd5405769b5f41960a7
Mohamed-Gamal-Mubarek <mohamedgamalmubarek@gmail.com>
2022-03-10 21:41:22 +0200

    edit import slugify

--- job-board/job/models.py
+++ job-board/job/models.py
@@ -1,6 +1,7 @@
 from distutils.command import upload
 from django.db import models
-from django.urls import slugify
+from django.utils.text import slugify
+
 
 # Create your models here.
 JOB_TYPE_CHOICE = [
