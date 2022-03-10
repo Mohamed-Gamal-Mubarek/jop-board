791afb2ffd24db6cdfafb7ef9e66216f9655fbc9
Mohamed-Gamal-Mubarek <mohamedgamalmubarek@gmail.com>
2022-03-10 21:09:42 +0200

    Pagintaion

--- job-board/job/views.py
+++ job-board/job/views.py
@@ -1,4 +1,4 @@
-
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -7,17 +7,24 @@ from .models import Job
 
 
 def jop_list(request):
+
     # GET ALL JOB BY THIS
     jobList = Job.objects.all()
-    # GET ALL JOB 
+    # GET ALL JOB
 
-    # STORE ALL JOB IN CONTEXT 
-    context = {'Jobs': jobList}
+    # PAGINATION WILL BE IN HERE
+    paginator = Paginator(jobList, 1)  # SHOW 1 jOB PER PAGE .
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    # STORE ALL JOB IN CONTEXT
+    context = {'Jobs': page_obj}
     # STORE ALL JOB IN CONTEXT
 
     # RETURN  ALL JOB TO RENDER DATA => RENDER DATA * , TEMPLATE YOU WANT TO VIEW , CONTEXT => SOTORE DATA
     # DATA IS ALL JOBS HERE
     return (render(request, 'job/jobs.html', context))
+
 
 def jop_details(request, id):
     # return( HttpResponse(f"this wil be an one job details by this {id}"))
