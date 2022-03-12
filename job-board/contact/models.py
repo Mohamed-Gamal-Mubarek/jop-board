c82c16327d880758fb7fd5d32b129e42a0a0d851
Mohamed-Gamal-Mubarek <mohamedgamalmubarek@gmail.com>
2022-03-12 17:59:52 +0200

    ok

--- job-board/contact/models.py
+++ job-board/contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# CREATING MODEL INFO
+class Info(models.Model):
+    place = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=11)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self) :
+        return self.email
+
