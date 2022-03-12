1ffd04c0162596fd3922d2c7e04d6ddbe82603f4
Mohamed-Gamal-Mubarek <mohamedgamalmubarek@gmail.com>
2022-03-12 19:01:54 +0200

    generate key

--- job-board/project/settings.py
+++ job-board/project/settings.py
@@ -139,3 +139,11 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = "mohamedgamalmubarek@gmail.com"
+EMAIL_HOST_PASSWORD = 'iwrpbiypizmoqqxm'
+EMAIL_US_TLS = True
+EMAIL_PORT = '587'
